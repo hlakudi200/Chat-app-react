fc61cd4592847594cfffdaf98261579ee4e2f535
hlakudi200 <tlhomphohlakudi@gmail.com>
2025-03-04 15:40:12 +0200

    hoc files cleanup

--- src/hoc/withAuth.tsx
+++ src/hoc/withAuth.tsx
@@ -2,8 +2,6 @@ import { Navigate } from "react-router";
 import { useAuth } from "../provider/authprovider/authProvider";
 
 const withAuth = (WrappedComponent: React.ComponentType) => {
- 
-
   return (props: any) => {
     const { user } = useAuth();
     if (user == null) {
